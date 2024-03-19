70c1decaa339cd3fd49105a9bd8e5ab37c891c69
ErnestoBorges <50407385+ernestoborges@users.noreply.github.com>
2024-03-19 17:57:31 +0000

    exercicio: Resolução de todos os exercicios 'CRUD'

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,13 @@
 -- Insira 3 classes salariais na tabela salario_classe
-
+INSERT INTO salario_classe(id_salario, nivel, salario)
+VALUES
+(1, 3500, 'estagiária'),
+(2, 4700, 'iniciante - grade 1'),
+(3, 5300, 'iniciante - grade 2');
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+
+INSERT INTO salario_classe(nivel, salario)
+VALUES
+(8500, 'gerencia'),
+(10700, 'coordenação de área');
